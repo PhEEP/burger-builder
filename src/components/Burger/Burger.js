1a15100401761249ce4c65ab5037531e85737cc2
Philippe Moore <philippealmoore@gmail.com>
2018-01-23 07:02:46 -0800

    burger building with default value when no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,20 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  // this is crazy
+  // from number of keys to arrays to elements with type of key
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
       });
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients</p>;
+    }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
