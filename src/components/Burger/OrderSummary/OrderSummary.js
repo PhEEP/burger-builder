f4fd1ee8f69672eebb39fbe3f8cd1fd7ca5f191c
Philippe Moore <philippealmoore@gmail.com>
2018-02-05 15:33:18 -0800

    order summary!

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Ox from '../../../hoc/Ox'
+import Ox from '../../../hoc/Ox';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -17,7 +18,10 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
-      <p>Continue to Chekout?</p>
+      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Ox>
   )
 }
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import Ox from '../../../hoc/Ox'
+import Ox from '../../../hoc/Ox';
 import Backdrop from '../Backdrop/Backdrop';
-import classes from './Modal.css'
+import classes from './Modal.css';
 
 const modal = (props) => (
   <Ox>
-  <Backdrop show={props.show} clicked={props.modalClosed}/>
-  <div 
-    style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}
-    className={classes.Modal}
-    >
-    {props.children}
-  </div>
+    <Backdrop show={props.show} clicked={props.modalClosed}/>
+    <div 
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0'
+      }}
+      className={classes.Modal}
+      >
+      {props.children}
+    </div>
   </Ox>
 );
 
