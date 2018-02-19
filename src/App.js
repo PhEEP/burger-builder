eb248898584d66f15ae0d3c0310b2a634b16b9fa
Philippe Moore <philippealmoore@gmail.com>
2018-02-19 07:03:21 -0800

    routing checkout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-
-import Layout from './hoc/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import { Route, Switch } from 'react-router-dom';
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 class App extends Component {
 
   render() {
     return ( 
-      <div >
+      <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <Switch>
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/checkout" component={Checkout} />
+          </Switch>
         </Layout>
       </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,29 +93,30 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({loading: true});
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Philippe Moore',
-        address: {
-          street: 'TestStreet 1',
-          zipCode: '12352',
-          country: 'USA'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    // is .json really necessary here?
-    axios.post('/orders.json', order)
-      .then(
-        this.setState({ loading: false, purchasing: false })
-      )
-      .catch(
-        this.setState({ loading: false, purchasing: false })
-      );
+    // this.setState({loading: true});
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Philippe Moore',
+    //     address: {
+    //       street: 'TestStreet 1',
+    //       zipCode: '12352',
+    //       country: 'USA'
+    //     },
+    //     email: 'test@test.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // // is .json really necessary here?
+    // axios.post('/orders.json', order)
+    //   .then(
+    //     this.setState({ loading: false, purchasing: false })
+    //   )
+    //   .catch(
+    //     this.setState({ loading: false, purchasing: false })
+    //   );
+    this.props.history.push('/checkout');
   }
 
   render () {
