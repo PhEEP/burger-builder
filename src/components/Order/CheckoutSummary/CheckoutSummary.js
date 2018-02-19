467c961f15dc85f2d06bc244a7d82b8e5a6ea277
Philippe Moore <philippealmoore@gmail.com>
2018-02-19 07:18:53 -0800

    query params to checkout form

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -4,6 +4,7 @@ import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
 import classes from './CheckoutSummary.css';
 const checkoutSummary = (props) => {
+  console.log(props, 'checkoutSummary');
   return (
     <div className={classes.CheckoutSummary}>
       <h1>We hope it tastes good!</h1>
@@ -12,10 +13,10 @@ const checkoutSummary = (props) => {
       </div>
       <Button 
         btnType="Danger"
-        clicked>CANCEL</Button>
+        clicked={props.checkoutCancelled}>CANCEL</Button>
       <Button 
         btnType="Success"
-        clicked>CONTINUE</Button>
+        clicked={props.checkoutContinued}>CONTINUE</Button>
     </div>
   )
 }
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,16 +4,38 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 class Checkout extends Component {
   state = {
     ingredients: {
-      salad: 1,
+      salad: 0,
       meat: 1,
       cheese: 1,
       bacon: 1
     }
   }
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {}
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ingredients: ingredients});
+    console.log(ingredients);
+  }
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients}/>
+        <CheckoutSummary 
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}/>
       </div>
     );
   }
