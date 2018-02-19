42bc2de3f1d85f64e49622f1c0d76ec796531666
Philippe Moore <philippealmoore@gmail.com>
2018-02-19 09:03:39 -0800

    Orders from firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,33 +93,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // this.setState({loading: true});
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Philippe Moore',
-    //     address: {
-    //       street: 'TestStreet 1',
-    //       zipCode: '12352',
-    //       country: 'USA'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-    // // is .json really necessary here?
-    // axios.post('/orders.json', order)
-    //   .then(
-    //     this.setState({ loading: false, purchasing: false })
-    //   )
-    //   .catch(
-    //     this.setState({ loading: false, purchasing: false })
-    //   );
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
