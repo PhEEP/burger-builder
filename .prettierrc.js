e935f490d9f272194c5504669a5f88d795ed7332
Philippe Moore <philippealmoore@gmail.com>
2018-04-08 12:14:42 -0700

    Adding firebase auth to project

--- .prettierrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  trailingComma: true,
-  singleQuote: true,
-  semi: false,
-}
---
+++ src/store/actions/auth.js
@@ -0,0 +1,48 @@
+import * as actionTypes from './actionTypes'
+import Axios from 'axios'
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  }
+}
+
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  }
+}
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  }
+}
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart())
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    }
+    let url =
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key='
+    if (!isSignup) {
+      url =
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key='
+    }
+    Axios.post(`${url}AIzaSyDceOvYCw-lmtQ7f2iO99nCvY23ppe8V2I`, authData)
+      .then(response => {
+        console.log(response)
+        dispatch(authSuccess(response.data))
+      })
+      .catch(err => {
+        console.log(err)
+        dispatch(authFail(err))
+      })
+  }
+}
