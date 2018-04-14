1062f98fff4436bf79261501a383f6f4dd9e572d
Philippe Moore <philippealmoore@gmail.com>
2018-04-14 10:55:28 -0700

    Workign on auto logout via redirect and a logout route

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from 'react'
 
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.css';
-import Backdrop from '../../UI/Backdrop/Backdrop';
-import Ox from '../../../hoc/Ox/Ox';
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
+import classes from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Ox from '../../../hoc/Ox/Ox'
 
-const sideDrawer = ( props ) => {
-    let attachedClasses = [classes.SideDrawer, classes.Close];
-    if (props.open) {
-        attachedClasses = [classes.SideDrawer, classes.Open];
-    }
-    return (
-        <Ox>
-            <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={attachedClasses.join(' ')}>
-                <div className={classes.Logo}>
-                    <Logo />
-                </div>
-                <nav>
-                    <NavigationItems />
-                </nav>
-            </div>
-        </Ox>
-    );
-};
+const sideDrawer = props => {
+  let attachedClasses = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open]
+  }
+  return (
+    <Ox>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems isAuthenticated={props.isAuth} />
+        </nav>
+      </div>
+    </Ox>
+  )
+}
 
-export default sideDrawer;
+export default sideDrawer
