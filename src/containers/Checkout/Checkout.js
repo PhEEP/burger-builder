276bc93bcf03ba85f24bb130edd7e3dafb2b54f9
Philippe Moore <philippealmoore@gmail.com>
2018-04-15 08:27:24 -0700

    removing console logs

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,26 +1,25 @@
-import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
-import ContactData from "./ContactData/ContactData";
-// import * as actions from '../../store/actions/index';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
   checkoutCancelledHandler = () => {
-    this.props.history.goBack();
-  };
+    this.props.history.goBack()
+  }
 
   checkoutContinuedHandler = () => {
-    this.props.history.replace("/checkout/contact-data");
-  };
+    this.props.history.replace('/checkout/contact-data')
+  }
 
   render() {
-    let summary = <Redirect to="/" />;
+    let summary = <Redirect to="/" />
     if (this.props.ings) {
       const purchasedRedirect = this.props.purchased ? (
         <Redirect to="/" />
-      ) : null;
+      ) : null
       summary = (
         <div>
           {purchasedRedirect}
@@ -30,21 +29,21 @@ class Checkout extends Component {
             checkoutContinued={this.checkoutContinuedHandler}
           />
           <Route
-            path={this.props.match.path + "/contact-data"}
+            path={this.props.match.path + '/contact-data'}
             component={ContactData}
           />
         </div>
-      );
+      )
     }
-    return summary;
+    return summary
   }
 }
 
 const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
-    purchased: state.order.purchased
-  };
-};
+    purchased: state.order.purchased,
+  }
+}
 
-export default connect(mapStateToProps)(Checkout);
+export default connect(mapStateToProps)(Checkout)
