c0e9bec11c41376ad53087e2e847386847028866
Philippe Moore <philippealmoore@gmail.com>
2018-05-02 20:48:28 -0700

    testing navigationItems

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,1 +1,15 @@
-describe('<NavigationItems />');
+import React from 'react'
+
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import NavigationItems from './NavigationItems'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+configure({ adapter: new Adapter() })
+
+describe('<NavigationItems />', () => {
+  it('should render 2 navigation items', () => {
+    const wrapper = shallow(<NavigationItems />)
+    expect(wrapper.find(NavigationItem)).toHaveLength(2)
+  })
+})
